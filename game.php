c6f4e990572f2791f5b4fbf2c6dbfbd0f0e38821
Slazenger <84316530+Slazenger1906@users.noreply.github.com>
2025-05-18 14:05:53 +0000

    change

--- game.php
+++ game.php
@@ -45,7 +45,7 @@ $result = check($computer, $human);
 <!DOCTYPE html>
 <html>
 <head>
-<title>Anamitra Musib 32ce3337</title>
+<title>Anamitra Musib 7955f62a </title>
 <?php require_once "bootstrap.php"; ?>
 </head>
 <body>
